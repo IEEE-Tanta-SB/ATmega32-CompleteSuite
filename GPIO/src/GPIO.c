58c4e810c494fd20cff4f76408d9219180b9e948
Mai-farahat <maifarahat00@gmail.com>
2024-07-08 00:02:32 +0300

    add mai_ws

---
+++ GPIO/src/GPIO.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : GPIO.c
+ Author      : Mai
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	/* Hello IEEE */
+	return EXIT_SUCCESS;
+}
