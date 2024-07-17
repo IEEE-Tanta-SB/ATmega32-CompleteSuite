3b3f5830ddba992c98827cf152da61fe85587de9
manal333 <142994550+manal333@users.noreply.github.com>
2024-07-18 02:53:04 +0300

    feat(interrupt):Add interrupt.h file

---
+++ 1_MCAL/3_INTERRUPT/interrupt.h
@@ -0,0 +1,61 @@
+/*
+ * interrupt.h
+ *
+ * Created: 7/15/2024 1:16:24 PM
+ *  Author: Manal Nabil
+ */
+
+
+#ifndef INTERRUPT_H_
+#define INTERRUPT_H_
+/*******************************************
+			 Section :INCLUDES
+*******************************************/
+
+#include "../4_Common/Platform_Config.h"
+
+/******************************************************************
+				Section :Macro Configuration References
+******************************************************************/
+
+#define NULL ((void*)0)
+
+#define LOW_LEVEL 0
+#define ANY_LOGICAL_CHANGE 1
+#define FALLING_EDGE 2
+#define RISING_EDGE 3
+
+#define INT_0 0
+#define INT_1 1
+#define INT_2 2
+
+void __vector_1(void) __attribute__((signal));
+void __vector_2(void) __attribute__((signal));
+void __vector_3(void) __attribute__((signal));
+
+/******************************************************************
+				Section :User Type Definition (Structures)
+******************************************************************/
+
+typedef struct
+{
+    uint8_t interrupt_channel;
+    uint8_t Sense_control;
+}external_interruptConfiguration_t;
+
+/******************************************************************
+				Section :APIS supported by "INTERRUPT driver"
+******************************************************************/
+
+void external_interrupts_enable();
+void external_interrupt0_enable();
+void external_interrupt1_enable();
+void external_interrupt2_enable();
+void external_interrupts_configuration(external_interruptConfiguration_t *confg);
+void external_interrupts_flag_status(external_interruptConfiguration_t *confg , uint8_t *state);
+void external_interrupts_flag_clear(uint8_t interrupt_channel);
+
+void EXTI_voidSetCallBack( void (*Copy_pvoidCallBack)(void) , external_interruptConfiguration_t *confg );
+
+
+#endif /* INTERRUPT_H_ */
