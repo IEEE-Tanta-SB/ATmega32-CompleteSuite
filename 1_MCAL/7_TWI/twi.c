3af16293b27d1142c8a5aa8ff7a7aa8de79d837d
manal333 <142994550+manal333@users.noreply.github.com>
2024-07-20 14:09:37 +0300

    feat(TWI): implement TWI_MasterInit function

--- 1_MCAL/7_TWI/twi.c
+++ 1_MCAL/7_TWI/twi.c
@@ -1,5 +1,44 @@
 /******************************************************************************
  * File: twi.c
  * Description: This file contains the implementation of TWI driver for ATmega32
- * Author:
+ * Author: Manal Nabil
  ******************************************************************************/
+
+#include "twi.h"
+
+/**
+ * @brief Initializes the TWI (Two Wire Interface) hardware as a master.
+ *
+ * This function sets up the TWI hardware in master mode with the specified prescaler.
+ *
+ * @param[in] Prescaler The prescaler value to configure the TWI clock.
+ */
+void TWI_MasterInit(uint8_t Prescaler)
+{
+	/*Set Bit Rate*/
+	TWBR= (uint8_t) (((F_CPU/SCL_Clock)-16) / (2*Prescaler));
+
+	/*Set Prescaller*/
+	if (Prescaler==PRESCALER_1)
+	{
+		TWSR=0;
+	}
+	else if(Prescaler==PRESCALER_4)
+	{
+		TWSR=1;
+	}
+	else if (Prescaler==PRESCALER_16)
+	{
+		TWSR=2;
+	}
+	else if (Prescaler==PRESCALER_64)
+	{
+		TWSR=3;
+	}
+
+	/*Enable Acknowledge bit*/
+	SET_BIT(TWCR,TWCR_TWEA);
+
+	/*Enable TWI*/
+	SET_BIT(TWCR,TWEN);
+}
