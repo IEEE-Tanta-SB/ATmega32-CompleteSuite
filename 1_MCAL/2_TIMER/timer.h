db94ff9108ed0f08f5b71a7220d36bbce6e1195f
mohamed-belall <mohamedbelal.eng@gmail.com>
2024-07-21 17:49:24 +0300

    feat(Timer): Macros Configuration References and APIs

--- 1_MCAL/2_TIMER/timer.h
+++ 1_MCAL/2_TIMER/timer.h
@@ -47,6 +47,51 @@ typedef struct
 }TIMER0_Config_t;
 
 
+/* ========================================================================================= */
+/* ============================= Macros Configuration References =========================== */
+/* ========================================================================================= */
+
+//-------------------------------------
+//		@ref Timer0_Mode_Define
+//-------------------------------------
+#define Timer0_Mode_Normal						(uint8_t)(0)		  // 0b0000
+#define Timer0_Mode_CTC							(uint8_t)(0x1<<WGM01) // 0b0001
+#define Timer0_Mode_PWM_NONINVERTING			(uint8_t)(0xD<<WGM01) // 0b1101
+#define Timer0_Mode_PWM_INVERTING				(uint8_t)(0xF<<WGM01) // 0b1111
+
+
+//-------------------------------------
+//		@ref Timer_Clock_Source_Define
+//-------------------------------------	
+#define Timer_Clock_Source_NO_CLOCK					(uint8_t)(0b000<<CS00)	
+#define Timer_Clock_Source_NO_PRESCALING			(uint8_t)(0b001<<CS00)
+#define Timer_Clock_Source_PRESCALING_8				(uint8_t)(0b010<<CS00)
+#define Timer_Clock_Source_PRESCALING_64			(uint8_t)(0b011<<CS00)
+#define Timer_Clock_Source_PRESCALING_256			(uint8_t)(0b100<<CS00)
+#define Timer_Clock_Source_PRESCALING_1024			(uint8_t)(0b101<<CS00)
+#define Timer_Clock_Source_External_Falling_Edge	(uint8_t)(0b110<<CS00)
+#define Timer_Clock_Source_External_Rising_Edge		(uint8_t)(0b111<<CS00)
+
+
+//-------------------------------------
+//		@ref IRQ_Enable_Define
+//-------------------------------------
+#define IRQ_Enable_NON								((uint8_t)(0))
+#define IRQ_Enable_OCIE0							((uint8_t)(1<<OCIE0))
+#define IRQ_Enable_TOIE0							((uint8_t)(1<<TOIE0))
+
+
+
+/* ========================================================================================= */
+/* ========================= APIs Supported by "MCAL Timer0 DRIVER" ======================== */
+/* ========================================================================================= */
+void MCAL_Timer0_Init(TIMER0_Config_t* timer0_cfg);
+void MCAL_Timer0_DeInit(void);
+
+void MCAL_Timer0_GetCounterValue(uint8_t* TicksNumber);
+void MCAL_Timer0_SetCompareValue(uint8_t TicksNumber);
+
+void MCAL_Timer0_PWM_DutyCycle(uint8_t DutyCycle);
 
 
 
